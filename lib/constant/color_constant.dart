326712e2023fa5a66c97b65689c1149c14c962eb
nishantGurutu <this.is.nishantkumar1@gmail.com>
2025-11-26 13:36:00 +0530

    new changes in color add const

--- lib/constant/color_constant.dart
+++ lib/constant/color_constant.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 
-final Color primary = Color(0xFF0D47A1);
-Color green = Colors.green;
-Color blue = Colors.blue;
-Color purple = Colors.deepPurple;
-Color background = Color(0xFFF5F5F5);
-Color darkGreyColor = Color.fromARGB(255, 134, 132, 132);
-Color lightBorderColor = Color.fromARGB(255, 137, 134, 134);
-Color mildBorderColor = Color.fromARGB(255, 198, 195, 195);
-Color whiteColor = Color(0xFFFFFFFF);
-Color blackColor = Color(0xFF000000);
-Color lightPink = Color(0xFFFFEEEE);
-Color lightGreen = Color(0xFFE5FFF9);
-Color lightGrey = Color(0xFFF4F4F4);
-Color lightBlue = Color(0xFFF5F7FF);
-Color lightMildGreen = Color(0xFFD1F1E4);
+const Color primary = Color(0xFF0F469A);
+const Color green = Colors.green;
+const Color blue = Colors.blue;
+const Color purple = Colors.deepPurple;
+const Color background = Color(0xFFF5F5F5);
+const Color darkGreyColor = Color.fromARGB(255, 134, 132, 132);
+const Color lightBorderColor = Color.fromARGB(255, 137, 134, 134);
+const Color mildBorderColor = Color.fromARGB(255, 198, 195, 195);
+const Color whiteColor = Color(0xFFFFFFFF);
+const Color blackColor = Color(0xFF000000);
+const Color lightPink = Color(0xFFFFEEEE);
+const Color lightGreen = Color(0xFFE5FFF9);
+const Color lightGrey = Color(0xFFF4F4F4);
+const Color lightBlue = Color(0xFFF5F7FF);
+const Color lightMildGreen = Color(0xFFD1F1E4);
 
 // Test type color
-Color primary1 = Color.fromARGB(255, 112, 163, 240);
-Color green1 = const Color.fromARGB(255, 159, 233, 161);
-Color blue1 = const Color.fromARGB(255, 84, 168, 237);
-Color purple1 = const Color.fromARGB(255, 166, 125, 237);
-Color lightMildGreen1 = Color.fromARGB(255, 171, 240, 212);
+const Color primary1 = Color.fromARGB(255, 112, 163, 240);
+const Color green1 = const Color.fromARGB(255, 159, 233, 161);
+const Color blue1 = const Color.fromARGB(255, 84, 168, 237);
+const Color purple1 = const Color.fromARGB(255, 166, 125, 237);
+const Color lightMildGreen1 = Color.fromARGB(255, 171, 240, 212);
