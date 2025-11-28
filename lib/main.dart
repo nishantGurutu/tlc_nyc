89914791d0afe5135570a4aced3e42a1616fecdf
nishantGurutu <this.is.nishantkumar1@gmail.com>
2025-11-28 20:35:27 +0530

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:tlc_nyc/constant/text_translation.dart';
+import 'package:tlc_nyc/firebase_options.dart';
 import 'package:tlc_nyc/routes/app_pages.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
