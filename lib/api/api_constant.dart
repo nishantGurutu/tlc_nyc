98e34708759a8a4b68b7dd2fac77f194a7d3a93a
nishantGurutu <this.is.nishantkumar1@gmail.com>
2025-12-07 12:05:00 +0530

    new api changes

--- lib/api/api_constant.dart
+++ lib/api/api_constant.dart
@@ -1,10 +1,13 @@
 class ApiConstant {
-  static const String baseUrl = "http://160.25.62.162:82/api/";
-  static const String addQuestionType =
-      "Master/AddQuestionType";
-  static const String questypeList = "Master/GetAllQuestionType";
-  static const String addQuestionWithAnswers =
-      "OnlineExam/AddQuestionWithAnswers";
-  static const String questionAnswerBytypeid =
-      "OnlineExam/GetQuestionWithAnswersByTypeCode";
+  static const String baseUrl = "https://tlc-backend-1.onrender.com/api/";
+  static const String register = "user/register";
+  static const String login = "user/login";
+  static const String addQuestionType = "questiontype/add";
+  static const String questypeList = "questiontype/list";
+  static const String updateQuestype = "questiontype/update";
+  static const String deleteQuestype = "questiontype/delete";
+  static const String addQuestionWithAnswers = "question/add";
+  static const String questionAnswerBytypeid = "question/list";
+  static const String updateQuestionAnswer = "question/update";
+  static const String deleteQuestionAnswer = "question/delete";
 }
